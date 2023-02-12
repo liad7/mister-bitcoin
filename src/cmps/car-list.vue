aeabc08a0def0ab1b45ae848ad8f9d7cae69565b
shrfrn <shrfrn@gmail.com>
2023-02-12 10:51:50 +0200

    List & Preview

---
+++ src/cmps/car-list.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <ul class="car-list">
+            <li v-for="car in cars" :key="car._id">
+                <CarPreview :car="car"/>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+import CarPreview from '@/cmps/car-preview.vue'
+export default {
+    // props: ['cars']
+    props: {
+        cars: {
+            type: Array,
+            required: true,
+        }
+    },
+    components: {
+        CarPreview,
+    }
+}
+</script>
+
+<style lang="scss"></style>
--- src/pages/car-index.vue
+++ src/pages/car-index.vue
@@ -1,9 +1,25 @@
 <template>
     <h1>Cars</h1>
+    <CarList v-if="cars" :cars="cars"/>
 </template>
 
 <script>
-export default {}
+import { carService } from '@/services/car.service.js'
+import CarList from '@/cmps/car-list.vue'
+
+export default {
+    data() {
+        return {
+            cars: null,
+        }
+    },
+    async created() {
+        this.cars = await carService.query()
+    },
+    components: {
+        CarList,
+    }
+}
 </script>
 
 <style lang="scss"></style>
