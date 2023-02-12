aaec97515a826b15239d29935b3080ee5d647d61
liad7 <liadgola7@gmail.com>
2023-02-12 14:08:39 +0200

    cleaning starter

--- src/App.vue
+++ src/App.vue
@@ -17,25 +17,3 @@ export default {
     <AppFooter />
 </template>
 
-<style scoped>
-main {
-    min-height: calc(90vh - 4rem);
-}
-nav a.router-link-exact-active {
-    color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
-}
-
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-    border: 0;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import HomeView from '../pages/HomeView.vue'
-import CarIndex from '../pages/car-index.vue'
-import CarDetails from '../pages/car-details.vue'
-import CarEdit from '../pages/car-edit.vue'
-
 const routerOptions = {
     history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
-        {
-            path: '/',
-            component: HomeView,
-        },
-        {
-            path: '/car',
-            component: CarIndex,
-        },
-        {
-            path: '/car/:_id',
-            component: CarDetails,
-        },
-        {
-            path: '/car/edit/:_id?',
-            component: CarEdit,
-        },
-        {
-            path: '/about',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import('../pages/AboutView.vue'),
-        },
     ],
 }
 const router = createRouter(routerOptions)
