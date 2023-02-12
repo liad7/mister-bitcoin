5d3080aa38ad652e50bc5eb19f0f7aa5a23ce835
liad7 <liadgola7@gmail.com>
2023-02-12 15:57:28 +0200

    contact list

--- src/cmps/contact-list.vue
+++ src/cmps/contact-list.vue
@@ -1,15 +1,16 @@
 <template>
-  <section class="contact-list">
-
+  <section class="contact-list" v-for="contact in contacts" :key="contact._id">
+    
   </section>
 </template>
 
 <script>
 export default {
-props:{
-    contacts:{
-
+  props: {
+    contacts: {
+      type: Array,
+      required: true,
     },
-},
-}
+  },
+};
 </script>
