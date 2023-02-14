c756273be1f98f73696ae2cb8b16b464b9712bd6
liad7 <liadgola7@gmail.com>
2023-02-14 15:12:25 +0200

    style btns

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,18 @@
 <template>
-  <section>Home</section>
+  <section class="home-page full main-layout">
+    <div class="wrapper">
+      <section class="home-contant">
+        <h1>{{ `Profitability\non the Rise` }}</h1>
+        <p>A trading platform that supports your financial goals.</p>
+        <RouterView to="/contact"
+          ><button class="btn">Get started</button></RouterView
+        >
+      </section>
+      <img src="src/assets/images/home-img.webp" alt="" />
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
